8599d78be1b56e57cddac5171b434950e5c2f130
Kirill <budanovkirilldmitrievich@gmail.com>
2022-08-22 22:39:50 +0300

    users

--- flask_blog/__init__.py
+++ flask_blog/__init__.py
@@ -2,9 +2,11 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_blog.config import Config
+from flask_bcrypt import Bcrypt
 
 db = SQLAlchemy()
 login_manager = LoginManager()
+bcrypt = Bcrypt()
 
 
 def create_app():
@@ -13,8 +15,11 @@ def create_app():
 
     db.init_app(app)
     login_manager.init_app(app)
+    bcrypt.init_app(app)
 
     from flask_blog.main.routes import main
+    from flask_blog.users.routes import users
     app.register_blueprint(main)
+    app.register_blueprint(users)
 
     return app
